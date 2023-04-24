a96b79c4ba00befa8d758806fe7da27bcb40473a
geoffrey kinuthia <geoffreykinuthia10@gmail.com>
2023-04-24 10:14:42 +0300

    final commit

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,13 +3,13 @@ import React from "react";
 // importing  Article component in "ArticleList.js"
 import Article from "./Article";
 
-// defining the ArticleList component function that accepts the prop articles.
+// defining the ArticleList component function that accepts the prop posts.
 // return contains array of Article components.
 // each Article component contain a prop of corresponding article in the "articles" array.
 function ArticleList({posts }) {
     return (
       <main>
-        {posts.map(post => (
+        {posts.map((post) => (
           <Article
             key={post.id}
             title={post.title}
